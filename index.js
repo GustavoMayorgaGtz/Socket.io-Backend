1ee59edc5ea96c5ff0b47a19eb48da9fe73b47c8
Gustavo Mayorga <redvioletoficial@gmail.com>
2022-08-03 17:59:02 -0500

    'save'

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const parser = require("body-parser");
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server)
 
+app.set('port', process.env.PORT || 3000);
+// habilitar cross origin
 app.use(cors());
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+//Generar fichero estatico para descarga
+app.use(express.static("./public"));
+/*************************************** */
+app.get("/", (req,res) => {
+    res.send("hola");
+})
+io.on("connection", (socket) => {
+    let identificador = socket.id;
+    console.log(identificador);
+   console.log("usuario conectado");
 
-app.listen(3000, () => {
+   io.on("disconnected", () => {
+    console.log("usuario desconectado");
+   })
+})
+
+/*************************************** */
+
+app.listen(app.get("port"), () => {
  console.log("server on")
+ console.log(app.get("port"))
 })
