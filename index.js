10f015ace801d84087ec666f2b87a13848cfcf70
Gustavo Mayorga <redvioletoficial@gmail.com>
2022-08-04 16:06:41 -0500

    save

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http,{
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      }
+});
 const cors = require("cors");
-const parser = require("body-parser");
-const http = require('http');
-const server = http.createServer(app);
-const {Server} = require("socket.io");
-const io = new Server(server)
-
-app.set('port', process.env.PORT || 3000);
-// habilitar cross origin
+const parser = require("body-parser")
 app.use(cors());
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
-//Generar fichero estatico para descarga
-app.use(express.static("./public"));
-/*************************************** */
-app.get("/", (req,res) => {
-    res.send("hola");
-})
-io.on("connection", (socket) => {
-    let identificador = socket.id;
-    console.log(identificador);
-   console.log("usuario conectado");
-
-   io.on("disconnected", () => {
-    console.log("usuario desconectado");
+//Whenever someone connects this gets executed
+io.on('connection', (socket) => {
+    const id = socket.id;
+    const {nameRoom} = socket.handshake.query;
+    socket.join(nameRoom);
+   console.log(`user connected with id ${id} in room ${nameRoom}`);
+   //Grupo test
+   socket.on("test", (res) => {
+     console.log("connected to test");
+   
+     socket.to(nameRoom).emit('test', res)
    })
-})
 
-/*************************************** */
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
 
-app.listen(app.get("port"), () => {
- console.log("server on")
- console.log(app.get("port"))
-})
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
